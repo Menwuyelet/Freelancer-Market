eea9c30f54354b1cec70557dd7dc4f8863008b26
Menu Temesgen <menutemesgen@gmail.com>
2026-01-03 15:34:44 +0300

    - I mplemented the HttpSessionConfigurator.java to enable access to HttpSession in WebSocket endpoints via handshake configurator

---
+++ src/main/java/com/project/websocket/HttpSessionConfigurator.java
@@ -0,0 +1,17 @@
+package com.project.websocket;
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
+
+// Access HTTP session for WebSocket
+public class HttpSessionConfigurator extends ServerEndpointConfig.Configurator {
+    @Override
+    public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
+        HttpSession httpSession = (HttpSession) request.getHttpSession();
+        if (httpSession != null) {
+            config.getUserProperties().put(HttpSession.class.getName(), httpSession);
+        }
+    }
+}
